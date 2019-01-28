0f4a2794a59dda8245e304c700835a683b77483c
Qamar <209461@dyn-160-39-223-182.dyn.columbia.edu>
2019-01-28 12:13:39 -0500

    next commit

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
 function setup() {
 	createCanvas(800,600); // make an HTML canvas element width x height pixels
 	frameRate(1)
+	ranh=Math.random()
+	ranm=Math.random()
+	rans=Math.random()
+	noStroke()
+
 
 }
 
 function draw() {
-	background(0, 0, 10);
+	background(240, 240, 255);
 	
 
 
@@ -13,26 +18,27 @@ function draw() {
 
 	for (var x = 0; x < hour(); x +=1) {
 
-		let xhrec = Math.random()*700;
-		fill(Math.random()*140,0, Math.random()*255);
-		rect(xhrec, 50, 10, 100) 
+		let xhrec = x*700;
+		fill(ranh*140,0, Math.random()*255);
+		rect(50+x*(750/12), 50, 10, 100) 
+
 
 	}	
 
 
 for (var x = 0; x < minute(); x +=1) {
 
-		let xmrec = Math.random()*700;
+		let xmrec = x*700;
 		fill(0, Math.random(.4,1)*255, 100);
-		rect(xmrec, 300, 5, 50) 
+		rect(50+x*(750/60), 300, 5, 50) 
 
 	}	
 
 for (var x = 0; x < second(); x +=1) {
 
-		let xsrec = Math.random()*700;
+		let xsrec = x*700;
 		fill(255, Math.random(0.8,1)*255, 0);
-		rect(xsrec, 500, 3, 30) 
+		rect(50+x*(750/60), 500, 3, 30) 
 
 	}	
 
